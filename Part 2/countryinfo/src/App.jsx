1f709d591cd989509a0838614206833dc98af5ce
Sam26047 <137920236+Sam26047@users.noreply.github.com>
2025-10-19 01:38:11 +0530

    Part 2.18 Complete

---
+++ Part 2/countryinfo/src/App.jsx
@@ -0,0 +1,99 @@
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+
+const CountryInfo=({country})=>{
+  const [countryInfo,setInfo] = useState(null)
+
+  const hook = ()=>{
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
+      .then(response=>{
+        const data=response.data
+        console.log(data)
+        setInfo(data)
+      })
+  }
+  useEffect(hook,[country])
+
+  if(!countryInfo) return (<></>)
+  
+  return(
+    <div>
+      <h1>{countryInfo.name.common}</h1>
+      <div>
+        {countryInfo.capital.map(x=>(<p key={x}>{x}</p>))}
+        <p>{countryInfo.region}</p>
+      </div>
+      <h2>Languages</h2>
+      <ul>
+        {Object.values(countryInfo.languages).map(lang => (
+          <li key={lang}>{lang}</li>
+        ))}
+      </ul>
+      <img src={countryInfo.flags.png} />
+    </div>
+  )
+}
+
+const Info = ({countries})=>{
+  if(countries.length>10){
+    return(
+      <p>
+        Too many matches,specify another filter
+      </p>
+    )
+  }
+  else if(countries.length===1){
+    return <CountryInfo country={countries[0]}/>
+  }
+  else{
+    return(
+      <div>
+        {countries.map(country=>{
+          return (<div key={country}>
+            {country} &nbsp;
+          </div>)
+        })}
+      </div>
+    )
+  }
+}
+
+
+function App(){
+  const [country,setCountry] = useState('')
+  const [countries,setCountries]  = useState([])
+
+  const hook = ()=>{
+    if(country==='') setCountries([])
+    else
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then(response=>{
+        const data =response.data
+        console.log(data);
+
+        const matches = data
+          .filter(match => match.name.common.toLowerCase().includes(country))
+          .map(match=>match.name.common);
+
+        setCountries(matches)
+        console.log(matches)
+      })
+      
+  }
+  useEffect(hook,[country])
+
+  const handleSearch= (event)=>{
+    setCountry(event.target.value)
+  }
+
+  return(
+    <div>
+      <p>find countries <input value={country} onChange={handleSearch} /></p>
+      <Info countries={countries} />
+    </div>
+  )
+}
+
+export default App
